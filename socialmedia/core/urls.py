e226a1b3dfbb3036ec2d109527a2540768c79ca4
Ula <urszula.zdanska@gmail.com>
2023-09-08 12:23:01 +0200

    Post Like

--- socialmedia/core/urls.py
+++ socialmedia/core/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup', views.signup, name='signup'),
     path('upload', views.upload, name='upload'),
+    path('like-post', views.like_post, name='like-post'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout'),
     path('settings', views.settings, name='settings'),
