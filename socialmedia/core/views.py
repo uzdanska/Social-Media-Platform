082fc18ff8baa5c87c1b9d7c3b65eaeac5ecf325
Ula <urszula.zdanska@gmail.com>
2023-09-08 23:36:35 +0200

    follow & unfollow section

--- socialmedia/core/views.py
+++ socialmedia/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.http import HttpResponse
-from .models import Profile, Post, LikePost
+from .models import Profile, Post, LikePost, FollowersCount
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
@@ -48,13 +48,46 @@ def profile(request, pk):
     user_posts = Post.objects.filter(user=pk)
     user_posts_length = len(user_posts)
 
+    follower = request.user.username
+    user = pk
+
+    if FollowersCount.objects.filter(follower=follower, user=user).first():
+        button_text = "Unfollow"
+    else:
+        button_text = "Follow"
+
+    user_followers = len(FollowersCount.objects.filter(user=pk))
+    user_following = len(FollowersCount.objects.filter(follower=pk))
+
     context = {
         'user_objects': user_objects,
         'user_profile': user_profile, 
         'user_posts': user_posts, 
-        'user_post_length': user_posts_length
+        'user_post_length': user_posts_length, 
+        'button_text': button_text, 
+        'user_followers':user_followers, 
+        'user_following': user_following
     }
     return render(request, 'profile.html', context)
+
+@login_required(login_url='signin')
+def follow(request):
+    if request.method == 'POST':
+        follower = request.POST['follower']
+        user = request.POST['user']
+        
+
+        if FollowersCount.objects.filter(follower=follower, user=user).first():
+            delete_follower = FollowersCount.objects.get(follower=follower, user=user)
+            delete_follower.delete()
+            return redirect('/profile/'+user)
+        else:
+            new_follower = FollowersCount.objects.create(follower=follower, user=user)
+            new_follower.save()
+            print('USER: ',user)
+            return redirect('/profile/'+user)
+    else:
+        return redirect('/')
 
 @login_required(login_url='signin')
 def upload(request):
