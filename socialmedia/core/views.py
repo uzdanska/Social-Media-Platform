17dede22ae9d5601cf49c0bad8b7070c6a34d81b
Ula <urszula.zdanska@gmail.com>
2023-09-08 11:49:11 +0200

    post feed

--- socialmedia/core/views.py
+++ socialmedia/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.http import HttpResponse
-from .models import Profile
+from .models import Profile, Post
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
@@ -10,7 +10,25 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='signin')
 
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'user_profile':user_profile, 'posts':posts})
+
+@login_required(login_url='signin')
+def upload(request):
+    if request.method == "POST":
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+
+        return redirect('/')
+    else:
+        return redirect('/') #home page
 
 @login_required(login_url='signin')
 def settings(request):
