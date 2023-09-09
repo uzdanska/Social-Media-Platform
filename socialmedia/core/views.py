8ff5c08e4a988281a801a10a69217022dcc9f730
Ula <urszula.zdanska@gmail.com>
2023-09-09 18:16:38 +0200

    post updated

--- socialmedia/core/views.py
+++ socialmedia/core/views.py
@@ -4,17 +4,29 @@ from django.http import HttpResponse
 from .models import Profile, Post, LikePost, FollowersCount
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
+from itertools import chain
 # Create your views here.
 
 @login_required(login_url='signin')
-
 def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
 
-    posts = Post.objects.all()
-    return render(request, 'index.html', {'user_profile':user_profile, 'posts':posts})
+    user_following_list = []
+    feed = []
+
+    user_following = FollowersCount.objects.filter(follower= request.user.username)
+
+    for users in user_following:
+        user_following_list.append(users.user)
+    
+    for usernames in user_following_list:
+        feed_lists = Post.objects.filter(user=usernames)
+        feed.append(feed_lists)
+
+    feed_list = list(chain(*feed))
+
+    return render(request, 'index.html', {'user_profile':user_profile, 'posts':feed_list})
 
 @login_required(login_url='signin')
 def like_post(request):
