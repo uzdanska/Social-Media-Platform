93b7c0d62e393a0cae5d513d6cd5ab38af95dc57
Ula <urszula.zdanska@gmail.com>
2023-09-09 18:52:15 +0200

    search user

--- socialmedia/core/urls.py
+++ socialmedia/core/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('upload', views.upload, name='upload'),
     path('follow', views.follow, name='follow'),
+    path('search', views.search, name='search'),
     path('profile/<str:pk>', views.profile, name='profile'),
     path('like-post', views.like_post, name='like-post'),
     path('signin', views.signin, name='signin'),
